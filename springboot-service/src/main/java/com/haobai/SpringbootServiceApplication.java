69af16c2b2917c64546f946d9ecf58ed01586d45
zjymlcjx <844937980@qq.com>
2018-11-09 19:00:00 +0800

    springboot

---
+++ springboot-service/src/main/java/com/haobai/SpringbootServiceApplication.java
@@ -0,0 +1,12 @@
+package com.haobai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootServiceApplication.class, args);
+	}
+}
